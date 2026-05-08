fc36dc2d84a3fea1196994361076df01e013387d
madhulatha <168632865+madhulathagudimella@users.noreply.github.com>
2026-05-08 16:05:35 +0530

    Time: 15 ms (82.46%), Space: 19.3 MB (54.26%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,24 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        s1_count = [0] * 26
+        window_count = [0] * 26
+
+        for c in s1:
+            s1_count[ord(c) - ord('a')] += 1
+
+        left = 0
+
+        for right in range(len(s2)):
+            window_count[ord(s2[right]) - ord('a')] += 1
+
+            if right - left + 1 > len(s1):
+                window_count[ord(s2[left]) - ord('a')] -= 1
+                left += 1
+
+            if s1_count == window_count:
+                return True
+
+        return False
