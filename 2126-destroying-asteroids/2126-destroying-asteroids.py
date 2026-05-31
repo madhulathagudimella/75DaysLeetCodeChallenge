62850fb799736723a2beaf5b2a95907564cecfb9
madhulatha <168632865+madhulathagudimella@users.noreply.github.com>
2026-05-31 20:49:14 +0530

    Time: 178 ms (10.34%), Space: 21.4 MB (51.72%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def asteroidsDestroyed(self, mass, asteroids):
+        asteroids.sort()
+
+        for asteroid in asteroids:
+            if mass < asteroid:
+                return False
+            mass += asteroid
+
+        return True
